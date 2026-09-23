feae9bbe586c14927ccac0db462093551c711d6b
Ryan1Dsouza <ryan1dsouza89@gmail.com>
2026-09-23 22:05:56 +0530

    fix: vercel deployment, routing, mobile 3d lag, and logo size

--- src/components/OrbitingSocials.tsx
+++ src/components/OrbitingSocials.tsx
@@ -61,7 +61,7 @@ interface OrbitingSocialsProps {
   theme: string;
 }
 
-export default function OrbitingSocials({ theme }: OrbitingSocialsProps) {
+export default function OrbitingSocials({}: OrbitingSocialsProps) {
   const socials = [
     {
       name: 'Instagram',
