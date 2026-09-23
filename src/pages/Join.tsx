3779a4c9364d3a29685bac303e79297a93564aed
Ryan1Dsouza <ryan1dsouza89@gmail.com>
2026-09-23 19:39:16 +0530

    chore: push all logo fixes and about page updates, excluding email connection links

--- src/pages/Join.tsx
+++ src/pages/Join.tsx
@@ -27,8 +27,8 @@ export default function Join() {
             Unlock opportunities, build real projects, and shape the future of technology.
           </p>
           <div className="join-buttons">
-              <a href="mailto:agentblazer@sjec.ac.in?subject=AgentBlazer%20Membership%20Application%20-%20%5BYour%20Name%5D&body=Hi%20AgentBlazer%20Team%2C%0D%0AI%20would%20like%20to%20join%20AgentBlazer%20as%20a%20member.%0D%0A%0D%0AWhy%20I%20want%20to%20join%3A%0D%0A%5BPlease%20explain%20why%20you%20want%20to%20join%20AgentBlazer%20and%20what%20you%20are%20interested%20in%20contributing.%5D%0D%0A%0D%0AGitHub%3A%0D%0A%5BPaste%20your%20GitHub%20profile%20link%5D%0D%0A%0D%0ALinkedIn%3A%0D%0A%5BPaste%20your%20LinkedIn%20profile%20link%5D%0D%0A%0D%0AThank%20you%2C%0D%0A%5BYour%20Name%5D" className="btn-primary">Become a Member</a>
-              <a href="mailto:cse.dept@sjec.ac.in?subject=AgentBlazer%20CSE%20Department%20Contact" className="btn-secondary">Contact CSE Department</a>
+            <button className="btn-primary">Become a Member</button>
+            <button className="btn-secondary">Contact CSE Department</button>
           </div>
         </div>
         
